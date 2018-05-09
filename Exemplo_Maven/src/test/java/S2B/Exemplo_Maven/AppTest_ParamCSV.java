21147c022e2cf693748babf4f20b3e942edf7df8
Lucas Plautz Prestes <lucas.plautz.prestes@gmail.com>
2018-05-09 16:03:29 -0300

    Fix Load File CSV

--- Exemplo_Maven/src/test/java/S2B/Exemplo_Maven/AppTest_ParamCSV.java
+++ Exemplo_Maven/src/test/java/S2B/Exemplo_Maven/AppTest_ParamCSV.java
@@ -4,31 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import junitparams.FileParameters;
 
 
+import static org.junit.Assert.assertEquals;
+import junitparams.JUnitParamsRunner;
 
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class AppTest_ParamCSV {
 
-	private int fInput_01;
-    private int fInput_02;
-    private int fExpected;
-
-    public AppTest_ParamCSV(int a, int b, int expected) {
-        fInput_01= a;
-        fInput_02= b;
-        fExpected= expected;
-    }
-	
+    
     @Test
-    @FileParameters("/Users/lucasplautzprestes/Documents/GitHub/S2B_Maven/file.csv")
+	@FileParameters("/Users/lucasplautzprestes/Documents/GitHub/S2B_Maven/file.csv")
+	public void loadParamsFromAnyFile(int a, int b, int expected) {
+		
+		assertEquals(expected, App.Soma(a,b));
+		
+	}
+    
+    /*
+    @Test
+	@FileParameters("/Users/lucasplautzprestes/Documents/GitHub/S2B_Maven/file.csv")
     public void loadParamsFromAnyFile(int a, int b, int output) {
     	
     	AppTest_ParamCSV obj = new AppTest_ParamCSV(a,b,output);
     	assertEquals(fExpected, App.Soma(obj.fInput_01, obj.fInput_02));
-    
     }
-    
+    */
 }
